2c281cb0ddbba1e08e4f3720aeb64a7459c8c547
Katarina Brdnik <katarina11brdnik@gmail.com>
2021-10-30 15:44:38 +0200

    dodelala model

--- didzeridu.py
+++ didzeridu.py
@@ -1,5 +1,4 @@
-#res se bom potrudila to naredit čim bolj kul
-
+import json
 class Zbirka:
     def __init__(self):
         self.izvajalci = []
@@ -24,5 +23,19 @@ class Zbirka:
     def seznam_vnosov(self):
         return self.vnosi
 
-
-
+    def shrani_podatke(self, datoteka): #da lahko shranim v json
+        vnos_v_slovar = {
+            "vnosi": [
+                {
+                    "izvajalec": vnos[0],
+                    "album": vnos[1],
+                    "žanri": vnos[2],
+                    "leto izida": vnos[3],
+                    "čas vnosa": vnos[4]
+                }
+                for vnos in self.vnosi
+            ]
+        }
+        with open(datoteka, "w", encoding="utf-8") as file:
+            json.dump(vnos_v_slovar, file, ensure_ascii=False, indent=4)
+#vnos se shrani v nabor oblike celoten_vnos = (vnos_izvajalca, vnos_albuma, vnos_zanra, leto_izida, cas_vnosa)
