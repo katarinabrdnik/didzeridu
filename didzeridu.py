7bd75b821fc08b710c6c61df9f8742b660524591
Katarina Brdnik <katarina11brdnik@gmail.com>
2021-10-30 15:44:14 +0200

    dodelala model

--- didzeridu.py
+++ didzeridu.py
@@ -6,11 +6,11 @@ class Zbirka:
         self.albumi = []
         self.vnosi = []
 
-    def dodaj_izvajalca(self, izvajalec):
-        self.izvajalci.append(izvajalec)
-
-    def dodaj_album(self, album):
-        self.albumi.append(album)
+#    def dodaj_izvajalca(self, izvajalec):
+#        self.izvajalci.append(izvajalec)
+#
+#    def dodaj_album(self, album):
+#        self.albumi.append(album)
 
     def dodaj_vnos(self, vnos):
         self.vnosi.append(vnos)
@@ -23,15 +23,6 @@ class Zbirka:
 
     def seznam_vnosov(self):
         return self.vnosi
-
-class Izvajalec:
-    def __init__(self, izvajalec, album, leto_izida):
-        self.izvajalec = izvajalec
-        self.album = album
-        self.leto_izida = leto_izida
-
-    def dodaj_album(self, izvajalec, album, leto_izida=""):
-        nov = Izvajalec(self, izvajalec, album, leto_izida)
 
 
 
